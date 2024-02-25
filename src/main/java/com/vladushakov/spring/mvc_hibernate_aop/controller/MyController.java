1a6494a0a0639db360f08dd3d57ff54c47789854
Vladi5lav <referall.2001@mail.ru>
2024-02-26 00:10:14 +0300

    addFeatureForUpdateExistingEmployee

--- src/main/java/com/vladushakov/spring/mvc_hibernate_aop/controller/MyController.java
+++ src/main/java/com/vladushakov/spring/mvc_hibernate_aop/controller/MyController.java
@@ -1,22 +1,43 @@
 package com.vladushakov.spring.mvc_hibernate_aop.controller;
 
-import com.vladushakov.spring.mvc_hibernate_aop.dao.EmployeeDAO;
 import com.vladushakov.spring.mvc_hibernate_aop.entity.Employee;
+import com.vladushakov.spring.mvc_hibernate_aop.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @Controller
 public class MyController {
     @Autowired
-    private EmployeeDAO employeeDAO;
+    private EmployeeService employeeService;
     @RequestMapping("/")
     public String showAllEmployees(Model model){
-        List<Employee> allEmployees = employeeDAO.getAllEmployees();
+        List<Employee> allEmployees = employeeService.getAllEmployees();
         model.addAttribute("allEmps", allEmployees);
         return "all-employees";
+    }
+
+    @RequestMapping("/add-new-employee")
+    public String addNewEmployee(Model model){
+        Employee employee = new Employee();
+        model.addAttribute("employee", employee);
+        return "employee-info";
+    }
+
+    @RequestMapping("/save-employee")
+    public String saveEmployee(@ModelAttribute("employee") Employee employee){
+        employeeService.saveEmployee(employee);
+        return "redirect:/";
+    }
+
+    @RequestMapping("/update-info")
+    public String updateEmployee(@RequestParam("empId") int id, Model model){
+        model.addAttribute("employee", employeeService.getEmployee(id));
+        return "employee-info";
     }
 }
